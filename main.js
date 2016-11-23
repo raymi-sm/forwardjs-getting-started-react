3ca7b03444fc0280bb923228cd91a7fd5292a2ba
Raymi Saldomando <raymi15@gmail.com>
2016-11-23 07:14:19 -0500

    notes on chapters 1 and 2 of Forward Course on Getting Started with React

--- main.js
+++ main.js
@@ -2,21 +2,22 @@ const jsContainer = document.getElementById('js');
 const reactContainer = document.getElementById('react');
 
 let counter = 0;
-
 jsContainer.innerHTML = `
   <div>
     <div>Hello JS</div>
+    <div>${new Date()}</div>
     <p id='counter'>${counter}</p >
     <input />
-  </div>
+    </div>
 `;
 setInterval(() => {
-// Creating an Element with the React API
   document.getElementById('counter').innerHTML = counter;
-  counter++;
+  counter++
+// Creating an Element with the React API
   ReactDOM.render(
     React.createElement('div', {},
       React.createElement('div', {}, 'Hello React'),
+      React.createElement('div', {}, new Date().toString()),
       React.createElement('input', {}),
       React.createElement('div', {id: 'counter'}, counter)
     ),
