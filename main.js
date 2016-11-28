9b27c4ced73a96f4c62e16b47541441477c7f721
Raymi Saldomando <raymi15@gmail.com>
2016-11-27 20:57:57 -0500

    the 3 ways of creating a React component

--- main.js
+++ main.js
@@ -1,15 +1,61 @@
 const reactContainer = document.getElementById('react');
+/*
+React takes all the "attributes" that you pass on your componenent ( in this case in <Counter /> and saves them inside a props object )
+
 const Counter = (props) => {
   return (
-    <p>{props.x}</p>
+    <div>
+      <span>{props.id}</span>: <span>{props.x}</span>
+    </div>
   );
 }
 
-const Counter2 = (props) => {
-  return(
-    React.createElement('p', null, props.x)
+But instead of constanly calling the props object we can destructured the variables and call them directly. This is also called atribute destructuring
+
+const Counter = (props) => {
+  let {id, x} = props
+  return (
+    <div>
+      <span>{id}</span>: <span>{x}</span>
+    </div>
   );
 }
+
+Shortcut way of doing this
+
+const Counter = ({ id, x }) => {
+  return (
+    <div>
+      <span>{id}</span>: <span>{x}</span>
+    </div>
+  );
+}
+
+*/
+
+// Creating a React component: First Way, the stateless function
+
+/*const Counter = ({ id, x }) => {
+  return (
+    <div>
+      <span>{id}</span>: <span>{x}</span>
+    </div>
+  );
+}*/
+
+// Creating a React component: Second Way
+
+const Counter = React.createClass({
+  render: function(){
+    let {id, x} = this.props;
+    return (
+      <div>
+        <span>{id}</span>: <span>{x}</span>
+      </div>
+    );
+  }
+})
+
 let x = 0;
 setInterval(() => {
   x++
@@ -17,8 +63,7 @@ setInterval(() => {
     <div>
       <div>Hello React</div>
       <input />
-      <Counter x={x} />
-      <Counter2 x={x} />
+      <Counter id="a6" x={x} />
     </div>
     ,
     reactContainer
